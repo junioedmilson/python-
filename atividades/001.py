b6d1a899d787cdd0e183326e764b5c7fcb4d408c
junioedmilson <junioedmilson2008@gmail.com>
2026-09-09 20:46:34 -0300

    atalização

--- atividades/001.py
+++ atividades/001.py
@@ -1,4 +1,4 @@
 # Atividade 001
 # Recebe o nome do usuário e imprime uma mensagem de boas-vindas
 nome = input('Qual seu nome? ')
-print (f'ola {nome}, seja bem vindo(a) meu github! ')
+print(f'ola {nome}, seja bem vindo(a) meu github! ')
